b264cd639875ec4cb8d1533993b1acde5cfe8d01
Sebastian Vittersø <sebastian.vitters@gmail.com>
2021-02-10 14:36:58 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="w-full h-screen">
+    <Three />
+  </div>
+</template>
+
+<script>
+import Three from './components/Three'
+
+export default {
+  name: 'App',
+  components: {
+    Three
+  }
+}
+</script>
+
+<style lang="scss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
